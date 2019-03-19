3aa34e9d730cf998b477edb4300a2bb7f96451c2
SungBeom <psb_94@naver.com>
2019-03-19 10:38:45 +0900

    Add info to DB

---
+++ FirebaseTest/Assets/Scripts/FirebaseTest.cs
@@ -0,0 +1,47 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User
+{
+    public string ID;
+    public string password;
+
+    public User() { }
+
+    public User(string ID, string password)
+    {
+        this.ID = ID;
+        this.password = password;
+    }
+}
+
+public class FirebaseTest : MonoBehaviour
+{
+    User user;
+    Dictionary<string, object> nextUser = new Dictionary<string, object>();
+    string json;
+
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://unitytest-a7eaa.firebaseio.com/");
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        user = new User("PSB", "123");
+        json = JsonUtility.ToJson(user);
+        reference.Child("USER").Child("SB").SetRawJsonValueAsync(json);
+
+        nextUser = new Dictionary<string, object>();
+        nextUser["ID"] = "CGH";
+        nextUser["password"] = "456";
+        reference.Child("USER").Child("GH").UpdateChildrenAsync(nextUser);
+
+        nextUser = new Dictionary<string, object>();
+        nextUser["ID"] = "KHS";
+        nextUser["password"] = "789";
+        reference.Child("USER").Child("HS").UpdateChildrenAsync(nextUser);
+    }
+}
---
+++ FirebaseTest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+    //public static GameManager Instance
+    //{
+    //    get
+    //    {
+    //        if (instance == null)
+    //        {
+    //            GameObject tempGM = new GameObject("GameManager");
+    //            instance = tempGM.AddComponent<GameManager>();
+    //            DontDestroyOnLoad(tempGM);
+    //        }
+    //        return instance;
+    //    }
+    //}
+
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
