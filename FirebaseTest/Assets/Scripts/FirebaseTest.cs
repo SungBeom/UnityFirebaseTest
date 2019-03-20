cd31abd2ea328f8a21d8ba32ea87f7f4feb81d93
SungBeom <psb_94@naver.com>
2019-03-20 21:48:09 +0900

    Get info from DB

--- FirebaseTest/Assets/Scripts/FirebaseTest.cs
+++ FirebaseTest/Assets/Scripts/FirebaseTest.cs
@@ -25,10 +25,39 @@ public class FirebaseTest : MonoBehaviour
     Dictionary<string, object> nextUser = new Dictionary<string, object>();
     string json;
 
+    void Awake()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp, i.e.
+                //   app = Firebase.FirebaseApp.DefaultInstance;
+                // where app is a Firebase.FirebaseApp property of your application class.
+
+                // Set a flag here indicating that Firebase is ready to use by your
+                // application.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
     void Start()
     {
+        // Set up the Editor before calling into the realtime database.
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://unitytest-a7eaa.firebaseio.com/");
+
+        // Get the root reference location of the database.
         DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        // 이는 앱 고유한 값을 가지게 됨, 이를 이용한 아이디와 비밀번호를 저장
+        Debug.Log(SystemInfo.deviceUniqueIdentifier);
+        // reference.Child("USER").Child(SystemInfo.deviceUniqueIdentifier).SetRawJsonValueAsync(json);
 
         user = new User("PSB", "123");
         json = JsonUtility.ToJson(user);
@@ -43,5 +72,30 @@ public class FirebaseTest : MonoBehaviour
         nextUser["ID"] = "KHS";
         nextUser["password"] = "789";
         reference.Child("USER").Child("HS").UpdateChildrenAsync(nextUser);
+
+        user = new User("Test", "t1");
+        json = JsonUtility.ToJson(user);
+        reference.Child(SystemInfo.deviceUniqueIdentifier).SetRawJsonValueAsync(json);
+
+        reference.Child("USER").GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted)
+                Debug.Log("failed");
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                Debug.Log(snapshot.Child("SB").Child("ID").Value);
+            }
+        });
+
+        reference.Child(SystemInfo.deviceUniqueIdentifier).GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted)
+                Debug.Log("failed");
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                Debug.Log(snapshot.Child("ID").Value);
+                Debug.Log(snapshot.Child("password").Value);
+            }
+        });
     }
 }
