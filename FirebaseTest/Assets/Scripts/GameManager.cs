52b68f3d7ce769c2d703ed5d9d20c265658e6705
SungBeom <psb_94@naver.com>
2019-03-24 21:38:52 +0900

    Make connection between app and firebaseDB #1

--- FirebaseTest/Assets/Scripts/GameManager.cs
+++ FirebaseTest/Assets/Scripts/GameManager.cs
@@ -34,6 +34,8 @@ public class GameManager : MonoBehaviour
         get { return reference; }
     }
 
+    ArrayList data = new ArrayList();
+
     void Awake()
     {
         if (instance)
@@ -45,52 +47,44 @@ public class GameManager : MonoBehaviour
         UDID = SystemInfo.deviceUniqueIdentifier;
         Debug.Log(UDID);
 
-        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
-        {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == DependencyStatus.Available)
-            {
-                // Create and hold a reference to your FirebaseApp, i.e.
-                //   app = Firebase.FirebaseApp.DefaultInstance;
-                // where app is a Firebase.FirebaseApp property of your application class.
-
-                // Set a flag here indicating that Firebase is ready to use by your
-                // application.
-            }
-            else
-            {
-                Debug.LogError(string.Format(
-                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-                // Firebase Unity SDK is not safe to use here.
-            }
-        });
-
         DontDestroyOnLoad(gameObject);
     }
 
     void Start()
     {
-        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://unitytest-a7eaa.firebaseio.com/");
-        FirebaseApp.DefaultInstance.SetEditorP12FileName("unitytest-a7eaa-ddb4aaa27e34-P12.p12");
-        FirebaseApp.DefaultInstance.SetEditorServiceAccountEmail("unitytest-a7eaa@appspot.gserviceaccount.com");
-        FirebaseApp.DefaultInstance.SetEditorP12Password("notasecret");
-
-        // Get the root reference location of the database.
-        reference = FirebaseDatabase.DefaultInstance.RootReference;
-
-        reference.Child(UDID).GetValueAsync().ContinueWith(initTask =>
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
         {
-            if (initTask.IsFaulted)
-                Debug.Log("failed");
-            else if (initTask.IsCompleted)
-            {
-                DataSnapshot snapshot = initTask.Result;
-
-                Debug.Log(snapshot.Child("Food").Value);
-                Debug.Log(snapshot.Child("Gold").Value);
-                Debug.Log(snapshot.Child("Jewelry").Value);
-            }
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+                InitializeFirebase();
+            else
+                Debug.LogError(string.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
         });
+
+
+        //// Set up the Editor before calling into the realtime database.
+        //FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://unitytest-a7eaa.firebaseio.com/");
+        ////FirebaseApp.DefaultInstance.SetEditorP12FileName("unitytest-a7eaa-ddb4aaa27e34.p12");
+        ////FirebaseApp.DefaultInstance.SetEditorServiceAccountEmail("unitytest-a7eaa@appspot.gserviceaccount.com");
+        ////FirebaseApp.DefaultInstance.SetEditorP12Password("notasecret");
+
+        //// Get the root reference location of the database.
+        //reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        //reference.Child(UDID).GetValueAsync().ContinueWith(initTask =>
+        //{
+        //    if (initTask.IsFaulted)
+        //        Debug.Log("failed");
+        //    else if (initTask.IsCompleted)
+        //    {
+        //        DataSnapshot snapshot = initTask.Result;
+
+        //        Debug.Log(snapshot.Child("Food").Value);
+        //        Debug.Log(snapshot.Child("Gold").Value);
+        //        Debug.Log(snapshot.Child("Jewelry").Value);
+        //    }
+        //});
 
         //if (reference != null)
         //{
@@ -112,6 +106,94 @@ public class GameManager : MonoBehaviour
         //{
         //    Debug.Log("Failed...");
         //}
+    }
+
+    void InitializeFirebase()
+    {
+        FirebaseApp app = FirebaseApp.DefaultInstance;
+        // NOTE: You'll need to replace this url with your Firebase App's database
+        // path in order for the database connection to work correctly in editor.
+        app.SetEditorDatabaseUrl("https://unitytest-a7eaa.firebaseio.com/");
+        if (app.Options.DatabaseUrl != null)
+            app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
+        StartListener();
+    }
+
+    void StartListener()
+    {
+        FirebaseDatabase.DefaultInstance.GetReference(UDID).
+            ValueChanged += (object sender, ValueChangedEventArgs e) =>
+            {
+                if (e.DatabaseError != null)
+                {
+                    Debug.LogError(e.DatabaseError.Message);
+                    return;
+                }
+                if (e.Snapshot != null && e.Snapshot.ChildrenCount > 0)
+                {
+                    foreach (var childSnapshot in e.Snapshot.Children)
+                    {
+                        //if (childSnapshot.Child("Food") == null
+                        //|| childSnapshot.Child("Food").Value == null)
+                        //{
+                        //    Debug.LogError("Bad data in sample.  Did you forget to call SetEditorDatabaseUrl with your project id?");
+                        //    break;
+                        //}
+                        //else
+                        //{
+                            Debug.Log("data : " +
+                                childSnapshot.Child("Food").Value.ToString() + " - " +
+                                childSnapshot.Child("Gold").Value.ToString() + " - " +
+                                childSnapshot.Child("Jewelry").Value.ToString());
+                            data.Insert(0, childSnapshot.Child("Food").Value.ToString()
+                                + " " + childSnapshot.Child("Gold").Value.ToString()
+                                + " " + childSnapshot.Child("Jewelry").Value.ToString());
+                        //}
+                    }
+                }
+            };
+    }
+
+    string food = "100";
+    string gold = "200";
+    string jewelry = "300";
+
+    public void AddData()
+    {
+        if (string.IsNullOrEmpty(food) || string.IsNullOrEmpty(gold) || string.IsNullOrEmpty(jewelry))
+        {
+            Debug.Log("fail...");
+            return;
+        }
+
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference(UDID);
+
+        reference.RunTransaction(AddDataTransaction).ContinueWith(task =>
+        {
+            if (task.Exception != null)
+                Debug.Log(task.Exception.ToString());
+            else if (task.IsCompleted)
+                Debug.Log("Transaction complete.");
+        });
+    }
+
+    TransactionResult AddDataTransaction(MutableData mutableData)
+    {
+        List<object> datas = mutableData.Value as List<object>;
+
+        if (datas == null)
+        {
+            datas = new List<object>();
+        }
+
+        Dictionary<string, object> newData = new Dictionary<string, object>();
+        newData["Food"] = food;
+        newData["Gold"] = gold;
+        newData["Jewelry"] = jewelry;
+        datas.Add(newData);
+
+        mutableData.Value = datas;
+        return TransactionResult.Success(mutableData);
     }
 
     class GoodsCount
--- FirebaseTest/Assets/Scripts/UpdateDB.cs
+++ FirebaseTest/Assets/Scripts/UpdateDB.cs
@@ -1,18 +1,19 @@
 ﻿using Firebase.Database;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UpdateDB : MonoBehaviour
 {
+    public Text log;
     public Text foodText;
     public Text goldText;
     public Text jewelryText;
 
     int foodCount, goldAmount, jewelryCount;
 
-    // Start is called before the first frame update
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(Click);
@@ -31,14 +32,21 @@ public class UpdateDB : MonoBehaviour
 
         GameManager.Instance.Reference.Child(GameManager.Instance.UDID).UpdateChildrenAsync(data);
 
+        log.text = data["Food"].ToString() + ", " + data["Gold"].ToString() + ", " + data["Jewelry"].ToString();
         GameManager.Instance.Reference.Child(GameManager.Instance.UDID).GetValueAsync().ContinueWith(task =>
         {
             if (task.IsFaulted)
+            {
+                log.text = "Failed...";
                 Debug.Log("failed");
+            }
             else if (task.IsCompleted)
             {
                 DataSnapshot snapshot = task.Result;
 
+                log.text = snapshot.Child("Food").Value.ToString() + "/" +
+                snapshot.Child("Gold").Value.ToString() + "/" +
+                snapshot.Child("Jewelry").Value.ToString();
                 Debug.Log(snapshot.Child("Food").Value);
                 Debug.Log(snapshot.Child("Gold").Value);
                 Debug.Log(snapshot.Child("Jewelry").Value);
